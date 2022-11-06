938036b64d9d24eb5d497a43d60c12c6ed4c25ba
Anastasia0404 <113851942+Anastasia0404@users.noreply.github.com>
2022-11-06 02:40:31 +0200

    Добавьте файлы проекта.

---
+++ lab5/Printer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lab5
+{
+    class Printer
+    {
+        public virtual void Print(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+    class PrinterRed : Printer
+    {
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            base.Print(value);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var printers = new Printer[2];
+            printers[0] = new Printer();
+            printers[1] = new PrinterRed();
+            printers[0].Print("It is Printer!");
+            printers[1].Print("It is PrinterRed!");
+            Console.ReadKey();
+        }
+    }
+}
